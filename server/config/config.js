d3e747fbc2a749451c06a62dd8e6b529f02317dd
hashHan <haeseong.han@gmail.com>
2018-03-25 20:17:40 +0900

    do not 'require('config.json')' for production mode.
    This cause problem with deployment because the production mode ignores config.json files.

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 console.log('env ****', env);
 
-if (env === 'development' || env === 'test' || env=='production') {
+if (env === 'development' || env === 'test') {
   var config = require('./config.json');
   var envConfig = config[env];
   Object.keys(envConfig).forEach((key) => {
