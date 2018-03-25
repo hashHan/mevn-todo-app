47b37033a18f51f97b31d10375798cb4c2c9bc53
hashHan <haeseong.han@gmail.com>
2018-03-25 19:35:06 +0900

    fix config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-var config = require('./config.json');
-var envConfig = config[env];
 
-Object.keys(envConfig).forEach((key) => {
-  process.env[key] = envConfig[key];
-});
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+
