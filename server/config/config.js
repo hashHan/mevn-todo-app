1b1b721d8a9b8c68e8f0988498b4af99601bf96e
hashHan <haeseong.han@gmail.com>
2018-03-25 20:27:18 +0900

    Add comments about config.json on production mode
    config.json do not exist on production mode.
    Therefore do not require it on production mode.
    SET environment on CLI for production mode.

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,9 @@ var env = process.env.NODE_ENV || 'development';
 
 console.log('env ****', env);
 
+//SET MONGODB_URI ON HEROKU CLI FOR PRODUCTION 'heroku config:set MONGODB_URI=<your-mlab-uri>'
 if (env === 'development' || env === 'test') {
-  var config = require('./config.json');
+  var config = require('./config.json'); // do not require this on production mode because it doesn't exist on productioin mode (.gitignore)
   var envConfig = config[env];
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
