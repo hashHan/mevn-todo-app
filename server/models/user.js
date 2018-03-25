8c8c810ae8bd3e05c9f711c2f8275a6949ecad52
hashHan <haeseong.han@gmail.com>
2018-03-25 16:44:00 +0900

    password hashing with bcript

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 //schema
 const UserSchema = new mongoose.Schema({
@@ -75,6 +76,21 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+//hashing password
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 //generate a model
 var User = mongoose.model('User', UserSchema);
