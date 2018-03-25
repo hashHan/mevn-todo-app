bd980830db84183104e5c963185bface1503ea98
hashHan <haeseong.han@gmail.com>
2018-03-25 18:57:39 +0900

    Set config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,8 @@
 var env = process.env.NODE_ENV || 'development';
 
-var db = {
-  dev: "mongodb://localhost:27017/TodoApp",
-  test: "mongodb://localhost:27017/TodoAppTest",
-  mlab: "mongodb://todoapp:todoapp@ds223019.mlab.com:23019/todoapp"
-};
+var config = require('./config.json');
+var envConfig = config[env];
 
-if (process.env.PORT) {
-  process.env.MONGODB_URI = db.mlab;
-} else {
-  process.env.PORT = 3000;
-  if (env === "development") {
-    process.env.MONGODB_URI = db.dev;
-  } else if (env === "test") {
-    process.env.MONGODB_URI = db.test;
-  }
-}
+Object.keys(envConfig).forEach((key) => {
+  process.env[key] = envConfig[key];
+});
